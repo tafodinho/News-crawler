dfd5d75eddad1943a041352a8d2536a2f4f580a5
Tafang Joshua <tafodinho@gmail.com>
2019-02-19 04:32:55 +0100

    first commit

---
+++ crawler/spiders/quotes_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'https://bbc.com',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
