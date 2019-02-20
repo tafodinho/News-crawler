e1423a54f1884378d978413faf58ca76ac343df3
Tafang Joshua <tafodinho@gmail.com>
2019-02-20 06:14:57 +0100

    added one spyder

--- crawler/spiders/quotes_spider.py
+++ crawler/spiders/quotes_spider.py
@@ -1,18 +1,79 @@
 import scrapy
+import json
+import re
+import mysql.connector
+import datetime
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
+    host="localhost"
+    user="root"
+    passwd="google"
+    database="bembits"
+
     def start_requests(self):
         urls = [
-            'https://bbc.com',
+            'https://www.journalducameroun.com/en/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    def mysql_connect(self):
+        return mysql.connector.connect(
+            host = self.host, 
+            user = self.user, 
+            passwd = self.passwd, 
+            database = self.database
+        )
+
     def parse(self, response):
+        links_crawled = []
         page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        filename = 'article-urls-%s.txt' % page
+        f = open(filename, 'w')
+
+        for articles in response.css("li > article"):
+            url = articles.css("a::attr(href)").get()
+
+            if url in links_crawled:
+                continue
+            try:
+                f.write(json.dumps({'url': url}))
+                f.write('\n')
+                links_crawled.append(url)
+                yield scrapy.Request(url=url, callback=self.parse1)
+            except:
+                pass
+        
         self.log('Saved file %s' % filename)
+        f.close()
+
+    def parse1(self, response):
+        article = response.css("article.post-full")
+
+        url = response.url
+        img = article.css("figure.post-img > img::attr(src)").get()
+        title = self.clean_string(article.css(".title::text").get())
+        date = self.clean_string(article.css("div.post-infos > p::text").get())
+        excerpt = article.css("div.post-content > p").get()
+        page = response.url.split("/")[-4]
+        insert_time = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+        
+        mydb = self.mysql_connect()
+        db_cursor = mydb.cursor()
+        sql1 = "SELECT * FROM cameroons WHERE title = '%s'" % title
+        db_cursor.execute(sql1)
+        result = db_cursor.fetchall()
+        if len(result) > 0:
+            return
+        sql = "INSERT INTO cameroons (name, image, title, excerpt, date, site, url, created_at, updated_at) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = ('cameroon', img, title, excerpt, insert_time, page, url, insert_time, insert_time)
+
+        db_cursor.execute(sql, val)
+        mydb.commit()
+        self.log('Saved data into DATABASE SUCCESS')
+        
+    def clean_string(self, mystring):
+        return re.sub('[\t\r\n]+', '', mystring)
+       
