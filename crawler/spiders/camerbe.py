9a8dfc198ef1c483388a7e7f88bebfda7966628f
Tafang Joshua <tafodinho@gmail.com>
2019-03-19 18:37:53 +0100

    added crawlers for camerbe

--- crawler/spiders/camerbe.py
+++ crawler/spiders/camerbe.py
@@ -6,13 +6,13 @@ import datetime
 from .database import Database
 
 
-class ActuCameroun(scrapy.Spider):
-    name = "actunews"
+class CamerBe(scrapy.Spider):
+    name = "camerbe"
     table = "cameroons"
     
     def start_requests(self):
         urls = [
-            'https://actucameroun.com/',
+            'https://www.camer.be/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -23,9 +23,10 @@ class ActuCameroun(scrapy.Spider):
         filename = 'urls-%s.txt' % page
         f = open(filename, 'w')
 
-        for articles in response.css(".td-block-span4"):
+        for articles in response.css(".card"):
             url = articles.css("a::attr(href)").get()
-            
+            if url[0] is '/':
+                url = response.url[:-1] + url
             if url in links_crawled:
                 continue
             try:
@@ -40,17 +41,17 @@ class ActuCameroun(scrapy.Spider):
         f.close()
 
     def parse1(self, response):
-        article = response.css("article")
+        article = response.css("article .card")
         print(response)
         url = response.url
-        image = article.css("div.td-post-featured-image img::attr(src)").get()
-        title = self.clean_string(article.css("h1.entry-title::text").get())
-        excerpt = article.css("div.td-post-content p::text").get()
-        date = self.clean_string(article.css("time::attr(datetime)").get())
+        image = article.css(".card img::attr(src)").get()
+        title = self.clean_string(article.css("h1.card-title::text").get())
+        excerpt = article.css("div.info p::text").getall()[2]
+        # date = self.clean_string(article.css("ul.authar-info li::text)").getall()[1])
         page = response.url.split("/")[2]
         insert_time = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
 
-        db = Database(url, image, title, excerpt, date, page, insert_time)
+        db = Database(url, image, title, excerpt, insert_time, page, insert_time)
         db.fill_db(self.table)
 
         self.log('Saved data into DATABASE SUCCESS')
